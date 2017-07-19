49c27ba70d682f67a58abe0e97638c5975541618
P GOVIND RAJ <p.govind.raj@gmail.com>
2017-07-19 17:32:27 +0530

    Changed Package.json

--- main.js
+++ main.js
@@ -77,7 +77,7 @@ autoUpdater.on('update-not-available', (info) => {
   sendStatusToWindow('Update not available.');
 })
 autoUpdater.on('error', (err) => {
-  sendStatusToWindow('Error in auto-updater.');
+  sendStatusToWindow('Error in auto-updater.' + err);
 })
 autoUpdater.on('download-progress', (progressObj) => {
   let log_message = "Download speed: " + progressObj.bytesPerSecond;
